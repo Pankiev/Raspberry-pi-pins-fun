90ed5447ad50ce5df63887784482fd9dad335401
Adrian Pankiewicz <farami11@tlen.pl>
2018-05-04 20:33:55 +0200

    no message

--- src/main/java/com/pi/pins/configuration/AsyncConfiguration.java
+++ src/main/java/com/pi/pins/configuration/AsyncConfiguration.java
@@ -4,24 +4,11 @@ import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 public class AsyncConfiguration implements AsyncConfigurer
 {
-	@Override
-	public Executor getAsyncExecutor() {
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(16);
-		executor.setMaxPoolSize(20);
-		executor.setQueueCapacity(96);
-		executor.initialize();
-		return executor;
-	}
-
 	@Override
 	public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
 		return new SimpleAsyncUncaughtExceptionHandler();
